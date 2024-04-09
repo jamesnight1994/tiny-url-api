c8d7fbff5750570ae78af4f155efe2d25c3eddd1
James <jamenight1994@gmail.com>
2024-04-09 03:55:16 +0300

    redirect to short url

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,23 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Res } from '@nestjs/common';
 import { AppService } from './app.service';
 import { ShortenUrlDto } from './app.dto';
+import { PrismaService } from './prisma/prisma.service';
+import { Response } from 'express';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService, private readonly prismaService: PrismaService) {}
 
   
   @Post('/shorten-url')
   shortenUrl(@Body() data: ShortenUrlDto) {
     return this.appService.shortenUrl(data);
+  }
+
+  @Get('/:short_url')
+  async getShortUrl(@Param('short_url') shortUrl: string, @Res() res: Response) {
+    const destination = await this.appService.getDestination(shortUrl);
+    
+    return res.redirect(destination);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -7,6 +7,18 @@ import { PrismaService } from './prisma/prisma.service';
 export class AppService {
   constructor(private prismaService: PrismaService) { }
 
+  async getDestination(shortUrl: string): Promise<string> {
+    // get short url destination
+    const url = await this.prismaService.url.findFirst({
+      where: {
+        short_url: shortUrl
+      }
+    });
+    const destination = url.destination;
+
+    return destination;
+  }
+
   async shortenUrl(data: ShortenUrlDto) {
     const short_url = MD5Utils.generateRandomShortUrl(data.long_url);
     const domain = process.env.APP_DOMAIN || 'http://localhost:3000';
